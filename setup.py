f1d33ca7c16152a19e171135fe62915f896d8749
Abdullah Arif <abdullahmeo11@gmail.com>
2021-03-26 22:04:20 -0400

    added files needed to become package

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+REQUIRED = ['beautifulsoup4', 'Jinja2', 'FacebookChatPhisher']
+setuptools.setup(
+    name="phishingTextGenerator", 
+    version="0.0.1",
+    author="Ashraf Taifour, Abdullah Arif, Abdullah Chattha, Abdullah Chattha, Steve Pham",
+    description="A program to generate phishing text based on the output of the Facebook Scraper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mohamadelchami/textGenerator",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=REQUIRED,
+    python_requires='>=3.7',
+)
